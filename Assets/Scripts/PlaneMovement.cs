3e7a5b90db35e461b23cd9b2f3dd67b478bd3acc
SheriffKovalski <m3llpak@gmail.com>
2023-08-12 17:30:02 +0300

    First commit

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    public float movX;
+    public float movY;
+    public float acceleration;
+    public float rotationControl;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movY = Input.GetAxis("Horizontal"); 
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Vector2 Vel = -transform.right * (movX * acceleration);
+        rb.AddForce(Vel);
+        rb.rotation += -movY * rotationControl;
+        Debug.Log(rb.velocity);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, 2);
+    }
+}
